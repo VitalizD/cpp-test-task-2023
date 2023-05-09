75bff0a5821b1f8bf80aa88734a959273466a82e
Vitaliy Dmitriev <71700778+VitalizD@users.noreply.github.com>
2023-05-09 14:11:28 +0500

    Update String.h

--- String.h
+++ String.h
@@ -5,24 +5,24 @@
 class String
 {
 public:
-	String(); // конструктор по умолчанию
-	String(const char* c_str); // конструктор для последовательности символов
-	String(const String& other); // конструктор копирования
-	String(String&& other) throw(); // конструктор перемещения
-	~String(); // деструктор
-	char* c_str() const; // C-строка
-	size_t length() const; // длина строки
-	String& operator=(const String& other); // оператор присваивания с копированием
-	String& operator=(String&& other) throw(); // оператор присваивания с перемещением
-	String& operator=(const char* c_str); // оператор присваивания с копированием (C-style)
-	String& operator+=(const String& other); // оператор инкремента
-	String& operator+=(const char* c_str); // оператор инкремента (C-style)
-	friend String operator+(const String& lvalue, const String& rvalue); // оператор сложения
-	friend String operator+(const String& lvalue, const char* rvalue); // оператор сложения (C-style)
-	friend std::ostream& operator<<(std::ostream& os, const String& str); // оператор сдвига
-	char operator[](unsigned index) const; // оператор индексирования
-	bool operator<(const String& other) const; // оператор "меньше"
-	bool operator>(const String& other) const; // оператор "больше"
+	String(); // Default constructor
+	String(const char* c_str); // Constructor for a sequence of characters
+	String(const String& other); // Copy constructor
+	String(String&& other) throw(); // Move constructor
+	~String(); // Destructor
+	char* c_str() const; // C-string
+	size_t length() const; // String length
+	String& operator=(const String& other); // Assignment operator with copying
+	String& operator=(String&& other) throw(); // Assignment operator with moving
+	String& operator=(const char* c_str); // Assignment operator with copying (C-style)
+	String& operator+=(const String& other); // Increment operator
+	String& operator+=(const char* c_str); // Increment operator (C-style)
+	friend String operator+(const String& lvalue, const String& rvalue); // Addition operator
+	friend String operator+(const String& lvalue, const char* rvalue); // Addition operator (C-style)
+	friend std::ostream& operator<<(std::ostream& os, const String& str); // Shift operator
+	char operator[](unsigned index) const; // Indexing operator
+	bool operator<(const String& other) const; // Less-than comparison
+	bool operator>(const String& other) const; // Greater-than comparison
 
 private:
 	char* data;
